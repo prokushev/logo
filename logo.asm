24a35a25be2474f5732c8abd5543ce2ad667f3d6
prokushev <yuri.prokushev@gmail.com>
2022-02-14 21:19:49 +0700

    test added

--- logo.asm
+++ logo.asm
@@ -2,6 +2,15 @@
 ; This is stub for Windows 1.01 logo
 ; Supported up to Win 3.xx (May be Win9x too? need to check) 
 ;
+; Logo API
+;
+; All code compiled as raw binary with offset 0.
+;
+; First 4 bytes must contain 'LOGO' signature
+; Entry table consist of 2 entry points:
+;  ShowLogo - Switche display to graphic mode, show logo and return last required byte of logo code in AX
+;  HideLogo - Switch display back to original mode
+;
 
 .8086
 			include	video.inc
@@ -12,11 +21,11 @@ code			segment
 			jmp	near ptr Init
 			jmp	near ptr Done
 CurrentVideoMode	db	?
-Done:			SetMode	[CurrentVideoMode]
+Done:			@SetMode [CurrentVideoMode]
 			ret
 ;--------------------------------------------------------
 DeadSpace:		
-Init:			GetMode
+Init:			@GetMode
 		        mov     [CurrentVideoMode],al
 			lea	dx, Hello
 			mov	ah,09H
--- make.cmd
+++ make.cmd
@@ -1,3 +1,7 @@
 del text.lgo
 jwasm -bin logo.asm
 ren logo.bin text.lgo
+
+jwasm -bin execlogo.asm
+
+copy execlogo.bin /b + text.lgo /b test.com
